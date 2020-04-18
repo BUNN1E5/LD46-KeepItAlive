08ffbda454c8ec9e239d4fa5e1958f8d459e62a4
Jacob <jacob@goldenneedham.com>
2020-04-18 14:11:31 -0700

    Changed camera cotnroller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,12 +13,15 @@ public class CameraController : MonoBehaviour
     public float distance = 12;
 
     public float LookSmoothness = 10;
+    public float TurnSmoothness = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    Vector3 goalPos = Vector3.zero;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -28,23 +31,31 @@ public class CameraController : MonoBehaviour
         Quaternion lookAt = Quaternion.LookRotation(player.transform.position - this.transform.position);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAt, LookSmoothness * Time.deltaTime);
 
-        float angleDiff = Vector3.Angle(-player.transform.forward, this.transform.forward);
-
-
         //The camera is free to move in a circle around the player
         //But the distance should be a fixed distance
         //Also the angle should be relative to the relative back of the vehicle
         //float _theta = theta * Mathf.Deg2Rad;
         //float _phi = phi * Mathf.Deg2Rad;
+        theta = Mathf.LerpAngle(theta, -(player.rotation.eulerAngles.y), TurnSmoothness * Time.deltaTime);
+        float _theta = (theta - 90) * Mathf.Deg2Rad ;
+        //TODO phi move the camera down a bit proportional to speed
 
-        theta = (-player.rotation.eulerAngles.y - 90) * Mathf.Deg2Rad;
+        goalPos = angle2Vec3(theta, phi) * distance;
 
+        this.transform.position = player.position + angle2Vec3(_theta, phi) * distance;
+        //this.transform.position = player.position;
+
+    }
+
+    Vector3 angle2Vec3(float theta, float phi){
         float x = Mathf.Cos (theta) * Mathf.Cos (phi);
         float y = Mathf.Sin (phi);
         float z = Mathf.Sin (theta) * Mathf.Cos (phi);
 
-        this.transform.position = player.position + new Vector3(x, y, z) * distance;
-        //this.transform.position = player.position;
+        return new Vector3(x, y, z);
+    }
 
+    void OnDrawGizmos(){
+        Gizmos.DrawWireSphere(goalPos, .5f);
     }
 }
