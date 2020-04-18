8956db59808b6e09ed2de55964074d363bc17683
Jacob <jacob@goldenneedham.com>
2020-04-18 13:23:05 -0700

    working on camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,13 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform player;
-    public Transform followPoint;
 
-    public float LookSmoothness = 6;
+    [Range(0, 6.28f)]
+    public float theta, phi = .68f;
+
+    public float distance = 12;
+
+    public float LookSmoothness = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +21,30 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         //We always want to look at the player
-        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookAt(player), LookSmoothness * Time.deltaTime);
+        Quaternion lookAt = Quaternion.LookRotation(player.transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAt, LookSmoothness * Time.deltaTime);
+
+        float angleDiff = Vector3.Angle(-player.transform.forward, this.transform.forward);
 
 
-    }
+        //The camera is free to move in a circle around the player
+        //But the distance should be a fixed distance
+        //Also the angle should be relative to the relative back of the vehicle
+        //float _theta = theta * Mathf.Deg2Rad;
+        //float _phi = phi * Mathf.Deg2Rad;
 
+        theta = player.rotation.eulerAngles.y * Mathf.Deg2Rad;
 
+        float x = Mathf.Cos (theta) * Mathf.Cos (phi);
+        float y = Mathf.Sin (phi);
+        float z = Mathf.Sin (theta) * Mathf.Cos (phi);
 
-    void FixedUpdate(){
-        //Cast a spherecast from the camera to the player to see if thery are visible
-        //If they are not visible move the camera to a position in which they will be
+        this.transform.position = player.position + new Vector3(x, y, z) * distance;
+        //this.transform.position = player.position;
 
-        //This can be calculated by 
     }
 }
