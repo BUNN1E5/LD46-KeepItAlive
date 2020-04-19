f91e355ae5a1c95e42e201012f85664bce5ef02a
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-04-18 17:45:57 -0700

    tires rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,18 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
 #pragma warning disable 0649
-    [SerializeField] float accel, steering, handbrake, maxVel, maxReverse;
+    [SerializeField] float accel, steering, handbrake, traction, maxVel, maxReverse, tireRot;
 #pragma warning restore 0649
 
     Rigidbody rb;
+    Transform[] tires;
     float vert, horiz;
     bool isGrounded, isHandbrakeOn;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        tires = GetComponentsInChildren<Transform>();
         isGrounded = true;
     }
 
@@ -25,24 +27,36 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-		isHandbrakeOn = Input.GetAxisRaw("Fire3") > 0f;
+        isHandbrakeOn = Input.GetAxisRaw("Fire3") > 0f;
         if (!isHandbrakeOn)
         {
             vert = Input.GetAxis("Vertical");
         }
-		horiz = Input.GetAxis("Horizontal");
+        horiz = Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate()
     {
         if (isGrounded)
         {
+			// acceleration
             if ((rb.velocity.magnitude < maxVel && vert > 0f) || (rb.velocity.magnitude < maxReverse && vert < 0f))
             {
                 rb.AddForce(transform.forward * accel * vert);
             }
-            rb.MoveRotation(rb.rotation * Quaternion.Euler(transform.up * (isHandbrakeOn ? handbrake : steering) * horiz));
+
+			// steering
+            if (isHandbrakeOn)
+            {
+                rb.MoveRotation(rb.rotation * Quaternion.Euler(transform.up * Mathf.Pow(handbrake, traction) * horiz * GetSpeedNormalized()));
+            }
+            else
+            {
+                rb.MoveRotation(rb.rotation * Quaternion.Euler(transform.up * steering * horiz));
+            }
         }
+
+        tires[2].localEulerAngles = tires[3].localEulerAngles = Vector3.up * tireRot * horiz;
     }
 
     void OnCollisionEnter()
@@ -55,11 +69,13 @@ public class PlayerController : MonoBehaviour
         isGrounded = false;
     }
 
-	public float GetSpeedNormalized() {
-		return  vert < 0f ? -rb.velocity.magnitude / maxReverse : rb.velocity.magnitude / maxVel;
-	}
+    public float GetSpeedNormalized()
+    {
+        return vert < 0f ? -rb.velocity.magnitude / maxReverse : rb.velocity.magnitude / maxVel;
+    }
 
-	public float GetAccelNormalized() {
-		return vert;
-	}
+    public float GetAccelNormalized()
+    {
+        return vert;
+    }
 }
